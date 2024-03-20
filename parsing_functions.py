21d708e90b476aa1be14c41dbeeb23f35199689b
vitaleeq <vitali.tsarev@gmail.com>
2024-03-20 18:19:53 +0300

    8th commit

--- parsing_functions.py
+++ parsing_functions.py
@@ -2,17 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 
 
+#url = 'https://www.alltime.ru'
 cookie = '''PHPSESSID=itd28p31t6vkrbpco5sack84r6; ALLTIME_SESSION_SEEN=f37c4bbc930295169cd75a60aa192c5f; rrpvid=216847436182733; _ga=GA1.1.2007646260.1709038380; rcuid=652d82a42169498a428027da; tmr_lvid=d0035b037b781b34ea8fc0715f39f444; tmr_lvidTS=1709038380508; _ym_uid=1709038381989044484; _ym_d=1709038381; rraem=; _userGUID=0:lt4dbqc0:lpX5y6QMnAM6xM6VWW5IMh_9Qg9MqcBK; adid=170903838167497; adrcid=AoB2f_r8dc6Io3-cZS-e4lg; rr-popup-shown=1; ALLTIME_CITY=54736769eec61c79f70f43079869af01; __utmc=260397277; __utmz=260397277.1710152346.3.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); smartbanner_exited=1; _ym_isad=2; g4c_x=1; srv_id=srv7; searchStartTime=1710437189704; _dvs=0:ltr4jlnd:uqhT78oFYSzeqBDcuRfu~oknsf2v4wXW; __utma=260397277.2007646260.1709038380.1710414353.1710437206.12; _ym_visorc=b; rr-subFormLastView=true; rrviewed=601234%2C666411; __utmb=260397277.6.9.1710437426928; tmr_detect=0%7C1710437434914; digi_uc=W1sidiIsIjYwMTIzNCIsMTcxMDQzNzgzNTgzM10sWyJ2IiwiNjY2NDExIiwxNzEwNDE0MzkzNDU3XSxbInYiLCI2Njg0NTkiLDE3MTA0MTAzODI4OThdLFsidiIsIjU1OTAzOCIsMTcxMDQwODYyOTEyOV0sWyJ2IiwiMzE3ODgwIiwxNzEwNDA4NTk1NTI5XSxbInYiLCI2MzM3MTIiLDE3MTAzMzI4MzYyMDZdLFsidiIsIjMxNzg3OSIsMTcxMDMzMjQxNTExM10sWyJ2IiwiNjQ3MDk1IiwxNzEwMjM3MjEzMjUwXSxbInYiLCI2Njg0NjEiLDE3MTAyMzY5Mzg5MDJdLFsidiIsIjUzNDY0NyIsMTcxMDIzNDk2MTIyNl0sWyJjdiIsIjU5NzM1MyIsMTcxMDQxMDM5NzI1MV0sWyJzdiIsIjYwMTIzNCIsMTcxMDQzNzE5MzIzMl0sWyJzdiIsIjYwMjc0NCIsMTcxMDIzMTg3ODEzMF0sWyJzdiIsIjY2ODQ1MyIsMTcxMDIzMTUwMzkyOF0sWyJzdiIsIjYzMzA5MSIsMTcxMDE2MDk3MTQ3Ml0sWyJzdiIsIjY2ODQ1OSIsMTcxMDE2MDkzNjYxMV1d; _ga_DV4C6GXRL9=GS1.1.1710437205.12.1.1710437850.60.0.0; rr-testCookie=testvalue; rr-viewItemId=601234; rrlevt=1710437850681'''
 cookie1 = '''PHPSESSID=itd28p31t6vkrbpco5sack84r6; ALLTIME_SESSION_SEEN=f37c4bbc930295169cd75a60aa192c5f; rrpvid=216847436182733; _ga=GA1.1.2007646260.1709038380; rcuid=652d82a42169498a428027da; tmr_lvid=d0035b037b781b34ea8fc0715f39f444; tmr_lvidTS=1709038380508; _ym_uid=1709038381989044484; _ym_d=1709038381; rraem=; _userGUID=0:lt4dbqc0:lpX5y6QMnAM6xM6VWW5IMh_9Qg9MqcBK; adid=170903838167497; adrcid=AoB2f_r8dc6Io3-cZS-e4lg; rr-popup-shown=1; __utmz=260397277.1710152346.3.3.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); g4c_x=1; __utmc=260397277; srv_id=srv4; _ym_isad=2; _dvs=0:ltx70xyk:DArF6Vdhaqa62xTgV6Tj0bQn30wEX49_; __utma=260397277.2007646260.1709038380.1710845048.1710850288.19; _ym_visorc=b; rr-subFormLastView=true; rrviewed=540023%2C668459%2C540024%2C628011%2C648080; __utmb=260397277.6.9.1710851104015; tmr_detect=0%7C1710851111999; digi_uc=W1sidiIsIjYyODAxMSIsMTcxMDg1MTEwMTUxNV0sWyJ2IiwiNTQwMDI0IiwxNzEwODUwMjg4MDgzXSxbInYiLCI2Njg0NTkiLDE3MTA4NDY3NDMxNDldLFsidiIsIjU1OTAzOCIsMTcxMDg0NTYwOTI4N10sWyJ2IiwiNTQwMDIzIiwxNzEwODQ1NDY5ODU1XSxbInYiLCI2NTAyMjIiLDE3MTA4NDQ2OTQ0MTNdLFsidiIsIjU5NzM1MyIsMTcxMDUwMDU0NDAzNF0sWyJ2IiwiNjUwMTU0IiwxNzEwNDkzNzA1NDE2XSxbInYiLCI2NTM5NDIiLDE3MTA0OTM0MzUwNjldLFsidiIsIjYwMTIzNCIsMTcxMDQ5MjYyMTk1N10sWyJjdiIsIjU5NzM1MyIsMTcxMDg0NTYxNTM2NV0sWyJzdiIsIjU0MDAyNCIsMTcxMDg1MDI4MzQ1Nl0sWyJzdiIsIjE4MDE3IiwxNzEwNzYzMDc5NTAzXSxbInN2IiwiNjAxMjM0IiwxNzEwNDM4MjQwMDM4XSxbInN2IiwiNjAyNzQ0IiwxNzEwMjMxODc4MTMwXSxbInN2IiwiNjY4NDUzIiwxNzEwMjMxNTAzOTI4XSxbInN2IiwiNjMzMDkxIiwxNzEwMTYwOTcxNDcyXSxbInN2IiwiNjY4NDU5IiwxNzEwMTYwOTM2NjExXSxbInYiLCI2NDgwODAiLDE3MTA4NTExNTY5ODhdXQ==; rr-testCookie=testvalue; rr-viewItemId=628011; rrlevt=1710852141648; _ga_DV4C6GXRL9=GS1.1.1710850287.20.1.1710852141.60.0.0'''
 #cookie_dynamic = requests.request("GET", url).headers['Set-Cookie']
 #cookies = 'srv_id=srv4'
+global_cookie = ''
+
+
+def get_cookie():
+    # try sessions later
+    url = 'https://www.alltime.ru'
+    response = requests.get(url)
+    global global_cookie
+    global_cookie = response.headers.get('Set-Cookie')
+    print(global_cookie)
+
 
 def get_product_id(vendor_code):
     url = 'https://www.alltime.ru/search/?NAME=' + vendor_code
+    print(url)
     page = requests.get(url)
-    #print(page.headers['Set-Cookie'])
-    #global cookie_dynamic
-    #cookie_dynamic = page.headers['Set-Cookie']
+    global global_cookie
+    get_cookie()
+    print(f'{global_cookie=}')
     soup = BeautifulSoup(page.text, "html.parser")
     try:
         id_number = soup.find('a', class_='catalog-item-link', href=True).get('href').split('/')[-2]
@@ -24,21 +37,8 @@ def get_product_id(vendor_code):
 def get_properties(id_number):
     try:
         url = 'https://www.alltime.ru/api/ajax/2020/product-mobile.php?ID=' + id_number + '&version=1.3'
-        headers = {'Cookie': cookie1}
+        headers = {'Cookie': global_cookie}
         response = requests.request("GET", url, headers=headers)
-        '''
-            if response.text == '':
-                print(f'{response.text=}')
-                if 'srv_id=srv4' in headers['Cookie']:
-                    headers['Cookie'] = headers['Cookie'].replace('srv_id=srv4', 'srv_id=srv7')
-                elif 'srv_id=srv7' in headers['Cookie']:
-                    headers['Cookie'] = headers['Cookie'].replace('srv_id=srv7', 'srv_id=srv4')
-                else:
-                    headers['Cookie'] += 'srv_id=srv4'
-            else:
-                print(f'{response.text=}')
-                break
-            '''
         soup = BeautifulSoup(response.text, 'html.parser')
         try:
             all_properties = get_info_from_page(soup)
